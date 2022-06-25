f69f748f44f3c631a4bd64cf3f869f94b38d16e2
Beacon24 <jdubin@thebeaconnetwork.org>
2022-06-25 14:04:46 -0400

    add start script

--- index.js
+++ index.js
@@ -131,7 +131,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('BEACON LIT ON PORT 3000'.rainbow)
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`BEACON LIT ON PORT ${PORT}`.rainbow)
 })
 
