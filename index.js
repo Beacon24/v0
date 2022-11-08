a732a254ad21505bb94ce35389bf49d3c87b10a3
Jared Dubin <jdubin@thebeaconnetwork.org>
2022-11-07 22:11:27 -0500

    sslify uninstalled

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-const http = require('http');
-const enforce = require('express-sslify');
+// const http = require('http');
+// const enforce = require('express-sslify');
+
 
 const mongoSanitize = require('express-mongo-sanitize');
 
@@ -58,10 +59,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+
 app.use(mongoSanitize({
     replaceWith: '_'
 }));
+
+// app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 const secret = process.env.SECRET || 'badsecret'
 
@@ -141,9 +144,9 @@ app.listen(port, () => {
     console.log(`BEACON LIT ON PORT ${port}`.rainbow)
 })
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function() {
+//     console.log('Express server listening on port ' + app.get('port'));
+// });
 
 
 
